f712b2bd67bb3c52ab6091134c2e030b6e6716cd
Danilo Araújo <daniloaraujo1990@hotmail.com>
2020-03-22 17:19:46 -0300

    CMake

---
+++ cmake-build-debug/CMakeFiles/freenic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/freenic.dir/src/main.cpp.o"
+  "freenic.pdb"
+  "freenic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/freenic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
